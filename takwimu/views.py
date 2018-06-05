f62333949882b654477e7bddc56f9b4cacb16d65
Phillip Ahereza <pahereza@gmail.com>
2018-06-05 16:56:11 +0300

    pulling displaying stories

--- takwimu/views.py
+++ takwimu/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import TemplateView
+from utils.medium import Medium
 
 
 class CountryReport(TemplateView):
@@ -6,3 +7,11 @@ class CountryReport(TemplateView):
     This view is for template debugging purposes
     """
     template_name = 'takwimu/report_page.html'
+
+    def get_context_data(self, **kwargs):
+        context = dict()
+        client = Medium()
+        stories = client.get_publication_posts('code-for-africa', count=6)
+        context['stories'] = stories
+        return context
+
