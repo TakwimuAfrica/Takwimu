e24314b41cb28a1366b94f7f7a27b5e491fd393f
Phillip Ahereza <pahereza@gmail.com>
2018-06-16 11:02:14 +0300

    added static psge

--- takwimu/urls.py
+++ takwimu/urls.py
@@ -3,9 +3,10 @@ from django.conf.urls.static import static
 from hurumap.urls import urlpatterns as hurumap_urlpatterns
 
 from takwimu import settings
-from takwimu.views import CountryReport
+from takwimu.views import CountryReport, ContactUsPage
 
 urlpatterns = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + \
               static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + \
               hurumap_urlpatterns + \
-              [ url(r'^report', CountryReport.as_view(), name='country_report')]
+              [ url(r'^report', CountryReport.as_view(), name='country_report'),
+                url(r'^contact_us', ContactUsPage.as_view(), name='country_report'), ]
