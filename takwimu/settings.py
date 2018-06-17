62128078ff81e39c1db1e312486ad991c257105b
David Lemayian <dlemayian@gmail.com>
2018-06-17 16:39:21 +0300

    Add debug toolboar.

--- takwimu/settings.py
+++ takwimu/settings.py
@@ -4,9 +4,15 @@ import os
 from hurumap.settings import *  # noqa
 
 # insert our overrides before both census and hurumap
-INSTALLED_APPS = ['takwimu'] + INSTALLED_APPS
+INSTALLED_APPS = ['takwimu'] + INSTALLED_APPS + ['debug_toolbar']
 
 ROOT_URLCONF = 'takwimu.urls'
+
+MIDDLEWARE_CLASSES = MIDDLEWARE_CLASSES + (
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+)
+
+INTERNAL_IPS = ['127.0.0.1', '172.18.0.1']
 
 
 # -------------------------------------------------------------------------------------
