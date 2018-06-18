ef21ec81788c363e02ebb9a589dba9771c7b1e03
Phillip Ahereza <pahereza@gmail.com>
2018-06-18 14:10:13 +0300

    display RSS feed

--- takwimu/feed.py
+++ takwimu/feed.py
@@ -10,13 +10,15 @@ class CountryReportFeed(Feed):
     description = "Updates on changes to country reports"
 
     def items(self):
-        return ReportPage.objects.order_by('-latest_revision_created_at')
+        return ReportPage.objects.live().order_by('-latest_revision_created_at')
 
     def item_title(self, item):
         return item.title
 
     def item_description(self, item):
-        return ''
+        return 'Country Report for {}'.format(item.title)
 
     def item_link(self, item):
-        return item.get_url()
+        return ''
+
+
