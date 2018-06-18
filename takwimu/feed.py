45c660a7a5f9e0cc9cf85fb1fd0c25b73d481553
Phillip Ahereza <pahereza@gmail.com>
2018-06-18 09:54:43 +0300

    added feed class

---
+++ takwimu/feed.py
@@ -0,0 +1,22 @@
+from django.core.urlresolvers import reverse
+from django.db import models
+from django.contrib.syndication.views import Feed
+from takwimu.models import ReportPage
+
+
+class CountryReportFeed(Feed):
+    title = "Country Reports Feed"
+    link = "/report-feed/"
+    description = "Updates on changes to country reports"
+
+    def items(self):
+        return ReportPage.objects.order_by('-latest_revision_created_at')
+
+    def item_title(self, item):
+        return item.title
+
+    def item_description(self, item):
+        return item.intro
+
+    def item_link(self, item):
+        return item.get_full_url()
