457953661b18d192eb0929ebbcb0476cdff678d1
Phillip Ahereza <pahereza@gmail.com>
2018-06-24 20:45:53 +0300

    add signal stub to process the dataset uploaded

--- takwimu/signals.py
+++ takwimu/signals.py
@@ -5,13 +5,4 @@ from takwimu.models import DataValue
 
 @receiver(post_save, sender=DataValue)
 def create_field_tables(sender, instance, created, **kwargs):
-    # get file saved
-
-    # read it into python
-
-    # check columns
-
-    # create a fieldtable???
-
-
-    pass
+    print "signal called"
