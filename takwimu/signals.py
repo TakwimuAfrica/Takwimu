34cf78e3710016d1be82fb1a78f4ccb4b2b35f35
Phillip Ahereza <pahereza@gmail.com>
2018-06-28 19:42:45 +0300

    save csv as json

--- takwimu/signals.py
+++ takwimu/signals.py
@@ -1,21 +1,47 @@
+from django.core.exceptions import ObjectDoesNotExist
 from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 from hurumap.models.data import DataIndicator
+from os.path import splitext
 import csv
 import json
 
-# @receiver(pre_save, sender=DataIndicator)
-# def convert_uploaded_csv_to_json(sender, instance, created, **kwargs):
-#     pass
+from wazimap.models import Geography
+
 
 @receiver(post_save, sender=DataIndicator)
 def create_field_tables(sender, instance, created, **kwargs):
-    print type(instance.raw_data_file.read())
-    if instance.raw_data_file:
-        reader = csv.DictReader(instance.raw_data_file.read())
-        rows = list(reader)
-        print '\n\n\n\n'
-        print rows
-        print '\n\n\n\n'
-    else:
-        print 'I just did nothing'
+    # to prevent post_save recursion, first disconnect the signal, the connect it again
+    # https://stackoverflow.com/questions/39481625/how-can-i-prevent-post-save-recursion-in-django
+    post_save.disconnect(create_field_tables, sender=sender)
+    path = instance.raw_data_file.path
+    _, extension = splitext(path)
+    data = convert_csv_to_json(path)
+
+    publisher_data = {
+        'type': extension,
+        'url_original': '',
+        'url_local': instance.raw_data_file.url
+    }
+
+    print data
+    print '\n\n\n\n'
+
+    instance.publisher_data = publisher_data
+    instance.data_values = data
+    instance.save()
+    post_save.connect(create_field_tables, sender=sender)
+
+
+def convert_csv_to_json(path):
+    data = []
+    with open(path) as f:
+        for row in csv.DictReader(f):
+            data.append(row)
+
+    return json.dumps(data)
+
+
+
+
+
