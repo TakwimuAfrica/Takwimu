5ad5459d44e143dc450c426e5936b87a82ad754d
Timothy Anthony <anthony.timothy90@gmail.com>
2018-07-02 15:01:57 +0300

    Registering other models

--- takwimu/translation.py
+++ takwimu/translation.py
@@ -1,6 +1,12 @@
-from .models import TopicPage
+from .models import TopicPageDataIndicator, TopicPage
 from modeltranslation.translator import TranslationOptions
 from modeltranslation.decorators import register
+
+@register(TopicPageDataIndicator)
+class TopicPageDataIndicatorTR(TranslationOptions):
+    fields = (
+        'indicator',
+    )
 
 @register(TopicPage)
 class TopicPageTR(TranslationOptions):
