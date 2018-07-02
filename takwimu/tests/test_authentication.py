321c1b3c8c0573158f5b862fb6b95474ef2b5123
Timothy Anthony <anthony.timothy90@gmail.com>
2018-07-02 12:34:54 +0300

    Adding language code in test_authentication

--- takwimu/tests/test_authentication.py
+++ takwimu/tests/test_authentication.py
@@ -1,12 +1,15 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
 
+from django.utils import translation
 
 class AuthenticationTest(TestCase):
+    language_code = translation.get_language()
+
     def test_login_endpoint_exists(self):
-        found = resolve('/accounts/login/')
+        found = resolve(language_code + '/accounts/login/')
         self.assertEqual(found.view_name, 'account_login')
 
     def test_signup_endpoint_exists(self):
-        found = resolve('/accounts/signup/')
+        found = resolve(language_code + '/accounts/signup/')
         self.assertEqual(found.view_name, 'account_signup')
