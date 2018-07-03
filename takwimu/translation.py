ef0d0c0106faf801c112d6163d21a768059e273c
Timothy Anthony <anthony.timothy90@gmail.com>
2018-07-03 10:56:05 +0300

    Registering ProfileSectionPage

--- takwimu/translation.py
+++ takwimu/translation.py
@@ -1,15 +1,15 @@
-from .models import TopicPageDataIndicator, TopicPage
+from .models import TopicPage, ProfileSectionPage
 from modeltranslation.translator import TranslationOptions
 from modeltranslation.decorators import register
 
-@register(TopicPageDataIndicator)
-class TopicPageDataIndicatorTR(TranslationOptions):
-    fields = (
-        'indicator',
-    )
-
 @register(TopicPage)
 class TopicPageTR(TranslationOptions):
+    fields = (
+        'description',
+    )
+
+@register(ProfileSectionPage)
+class ProfileSectionPageTR(TranslationOptions):
     fields = (
         'description',
     )
