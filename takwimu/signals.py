dca76c8cb362440d7611b15fc670457cd50cf565
Phillip Ahereza <pahereza@gmail.com>
2018-07-03 17:49:21 +0300

    strip whitespaces from both ends of geo name. Excel seems to add whitespace to the start

--- takwimu/signals.py
+++ takwimu/signals.py
@@ -18,21 +18,8 @@ def create_field_tables(sender, instance, created, **kwargs):
         return
     post_save.disconnect(create_field_tables, sender=sender)
     path = instance.raw_data_file.path
-    data_values = {}
-    try:
-        df = pd.read_csv(path)
-
-        # get disaggregation column
-        disaggregation_column = \
-        list(set(df.columns) - set(['geography', 'year', 'value']))[0]
-
-
-
-    except:
-        return
-
+    data_values = return_data_values(path, instance.title)
     _, extension = splitext(path)
-    data = convert_csv_to_json(path)
 
     publisher_data = {
         'type': extension,
@@ -40,11 +27,12 @@ def create_field_tables(sender, instance, created, **kwargs):
         'url_local': instance.raw_data_file.url
     }
 
-    print data
-    print '\n\n\n\n'
+    print '\n\n\n\n\n'
+    print data_values
+    print '\n\n\n\n\n'
 
     instance.publisher_data = publisher_data
-    instance.data_values = data
+    instance.data_values = json.dumps(data_values)
     instance.save()
     post_save.connect(create_field_tables, sender=sender)
 
@@ -60,7 +48,7 @@ def convert_csv_to_json(path):
 
 def get_geo_data(geo_name):
     try:
-        query = Geography.objects.get(name__iexact=geo_name)
+        query = Geography.objects.get(name__iexact=geo_name.strip())
         return '-'.join([query.geo_level, query.geo_code])
     except ObjectDoesNotExist:
         return 'undefined'
@@ -93,9 +81,62 @@ def get_stats_per_year(df, aggfunc):
 
 
 def get_data_values_per_geography(df, disagg_column):
+    data = {}
     for key, df_key in df.groupby(['geography', disagg_column]):
-        pass
-    pass
+        data[key] = dict(zip(df_key.year, df_key.value))
+    return data
+
+
+def return_data_values(path, indicator_title):
+    data_values = {}
+    try:
+        df = pd.read_csv(path)
+        df['geography'] = df['geography'].apply(lambda x: get_geo_data(x))
+
+        # get disaggregation column
+        disaggregation_column = \
+            list(set(df.columns) - {'geography', 'year', 'value'})[0]
+
+        totals = get_stats_per_year(df, aggfunc=np.sum)
+        average = get_stats_per_year(df, aggfunc=np.mean)
+
+        grouped_values = get_data_values_per_geography(df,
+                                                       disaggregation_column)
+
+        for key, value in grouped_values.iteritems():
+            # (disaggregation_column, geo)
+            # check if the geography doesn't exist
+            if not key[1] in data_values.keys():
+                data_values[key[1]] = {key[0]: {
+                    'name': indicator_title,
+                    '_default': 0,
+                    'values': value
+                }
+                }
+            else:
+                data_values[key[1]][key[0]] = {
+                    'name': indicator_title,
+                    '_default': 0,
+                    'values': value
+                }
+
+        # add totals
+        for geo, value in totals:
+            if not geo in data_values.keys():
+                data_values[geo] = {'_totals': value}
+            else:
+                data_values[geo]['_totals'] = value
+
+        # add averages
+        for geo, value in average:
+            if not geo in data_values.keys():
+                data_values[geo] = {'_totals': value}
+            else:
+                data_values[geo]['_totals'] = value
+
+    except Exception as e:
+        return data_values
+
 
 """
 TODO 
@@ -105,4 +146,8 @@ TODO
  - get sum of all dataframes by `year' to get total
  - get average and other stats if necessary - using inbuilt pandas methods
  - format data and save
+"""
+
+"""
+
 """
