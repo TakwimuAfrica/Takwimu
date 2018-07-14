e28ab2a3b8abbbbd581dbaa37509e64066d56bd2
Phillip Ahereza <pahereza@gmail.com>
2018-07-14 12:56:47 +0300

    remove invalid import

--- takwimu/models/__init__.py
+++ takwimu/models/__init__.py
@@ -1,4 +1,3 @@
 from .dashboard import TopicPageDataIndicator, TopicPageDataIndicators, TopicPage, \
                        ProfileSectionPage, ProfilePageSection, ProfilePageSections,\
                        ProfilePage
-from .settings import ReachUsSettings, AboutUsSettings
