37ac7431373f1a8ffca5f52af43b9c4391089bb0
David Lemayian <dlemayian@gmail.com>
2018-07-23 21:47:00 +0300

    Add cover image to support services.

--- takwimu/models/dashboard.py
+++ takwimu/models/dashboard.py
@@ -8,6 +8,9 @@ from wagtail.wagtailadmin.edit_handlers import FieldPanel, StreamFieldPanel, Pag
 from wagtail.wagtailcore import blocks
 from wagtail.wagtailembeds.blocks import EmbedBlock
 from wagtail.wagtaildocs.blocks import DocumentChooserBlock
+
+from wagtail.wagtailimages.models import Image
+from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
 from wagtail.wagtailimages.blocks import ImageChooserBlock
 
 from wagtail.wagtailcore.fields import StreamField, RichTextField
@@ -17,8 +20,8 @@ from modelcluster.fields import ParentalKey
 
 from wazimap.models import Geography
 from hurumap.models import DataTopic, DataIndicator
-from fontawesome.fields import IconField #importing property from djano-fontawesome app for icons on TopicPage
-from fontawesome.forms import IconFormField #importing property from djano-fontawesome app for icon field on TopicBlock
+from fontawesome.fields import IconField  # importing property from djano-fontawesome app for icons on TopicPage
+from fontawesome.forms import IconFormField  # importing property from djano-fontawesome app for icon field on TopicBlock
 
 import logging
 logger = logging.getLogger(__name__)
@@ -294,12 +297,27 @@ class Service(models.Model):
 
     title = models.TextField()
     icon = IconField()
+    cover = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
     description = RichTextField()
     category = models.CharField(
         max_length=20,
         choices=SERVICE_CATEGORIES,
         default='Standard'
     )
+
+    panels = [
+        FieldPanel('title'),
+        FieldPanel('icon'),
+        ImageChooserPanel('cover'),
+        FieldPanel('description'),
+        FieldPanel('category'),
+    ]
 
     def get_slug(self):
         # remove special characters and punctuation
