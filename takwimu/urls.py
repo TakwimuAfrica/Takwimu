8dff8f38c543beee8b675583f4bbe9e75c274273
Phillip Ahereza <pahereza@gmail.com>
2018-07-24 03:05:08 +0300

    404 and 500 pages

--- takwimu/urls.py
+++ takwimu/urls.py
@@ -14,7 +14,7 @@ urlpatterns = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + \
               [
                   url(r'^$', HomePageView.as_view(), name='home'),
                   url(r'^about/support-services', SupportServicesIndexView.as_view(), name='about_support_services'),
-                  url(r'^about/?$', AboutUsView.as_view(), name='about_page'),
+                  url(r'^about/$', AboutUsView.as_view(), name='about_page'),
                   url(r'^feed/$', CountryProfileFeed(), name='rss_feed'),
                   url(r'^profiles/$', ProfileView.as_view(), name='profiles'),
               ] + \
@@ -25,3 +25,6 @@ if settings.DEBUG:
     urlpatterns = [
         url(r'^__debug__/', include(debug_toolbar.urls)),
     ] + urlpatterns
+
+handler404 = 'takwimu.views.handler404'
+handler500 = 'takwimu.views.handler500'
--- takwimu/views.py
+++ takwimu/views.py
@@ -2,6 +2,8 @@ import json
 import operator
 
 from django.conf import settings
+from django.shortcuts import render_to_response
+from django.template import RequestContext
 from django.views.generic import TemplateView, ListView
 
 from takwimu.models.dashboard import Service, ExplainerSteps, FAQ, Testimonial
@@ -40,3 +42,17 @@ class AboutUsView(TemplateView):
         context = super(AboutUsView, self).get_context_data(**kwargs)
         context['support_services'] = Service.objects.all().filter(category="Persona")
         return context
+
+
+def handler404(request):
+    response = render_to_response('404.html', {},
+                                  context_instance=RequestContext(request))
+    response.status_code = 404
+    return response
+
+
+def handler500(request):
+    response = render_to_response('500.html', {},
+                                  context_instance=RequestContext(request))
+    response.status_code = 500
+    return response
