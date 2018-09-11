fde3f86d26b45a1c44fc572f96253e633a68cbde
Phillip Ahereza <pahereza@gmail.com>
2018-09-11 05:22:45 +0300

    show level1 geography names .i.e region, state or county

--- takwimu/templatetags/geotags.py
+++ takwimu/templatetags/geotags.py
@@ -8,9 +8,30 @@ CENSUS = {
     'senegal': '2013'
 }
 
+LEVEL1_NAMES = {
+    'tanzania': 'Region',
+    'kenya': 'County',
+    'nigeria': 'State',
+    'senegal': 'Region'
+}
+
 
 @register.filter
 def census_year(country):
     return CENSUS.get(country.lower(), 'Unknown')
+
+
+@register.filter
+def geo_level(geo):
+    current_geo = geo.get('this')
+    if current_geo.get('geo_level') == 'level1':
+        try:
+            country_name = geo['parents']['country']['short_name']
+            return LEVEL1_NAMES.get(country_name.lower(), 'Level1')
+        except KeyError:
+            return 'Level1'
+    else:
+        return current_geo['geo_level'].title()
+
 
 
