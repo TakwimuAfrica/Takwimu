070a0ce77d06a14e9f5632424b1bd44cf9612b3c
Phillip Ahereza <pahereza@gmail.com>
2018-11-08 14:08:21 +0300

    use topic title as target

--- takwimu/models/dashboard.py
+++ takwimu/models/dashboard.py
@@ -1,8 +1,10 @@
+import json
 import re
 import warnings
 
 from django.db import models
 from django import forms
+from django.utils.text import slugify
 from wagtail.contrib.settings.models import BaseSetting, register_setting
 from wagtail.wagtailadmin.edit_handlers import FieldPanel, StreamFieldPanel, \
     PageChooserPanel, InlinePanel
@@ -29,6 +31,11 @@ from fontawesome.forms import IconFormField
 import logging
 
 logger = logging.getLogger(__name__)
+
+json_data = open('takwimu/fixtures/sdg.json')
+sdg_data = json.load(json_data)
+sdg_choices = [(slugify(i.get('short')), i.get('short')) for i in sdg_data]
+sdg_choices = [('', 'Please select an SDG')] + sdg_choices
 
 
 # The abstract model for data indicators, complete with panels
@@ -137,6 +144,8 @@ class IndicatorWidgetsBlock(blocks.StreamBlock):
             ('title', blocks.CharBlock(required=False)),
             ('hide_title', blocks.BooleanBlock(default=False, required=False)),
             ('body', blocks.RichTextBlock(required=False)),
+            ('sdg', blocks.ChoiceBlock(required=False, choices=sdg_choices,
+                                       label='SDG Goal')),
             ('source', blocks.RichTextBlock(
                 features=['link'], required=False)),
         ],
@@ -153,6 +162,8 @@ class IndicatorWidgetsBlock(blocks.StreamBlock):
             ('title', blocks.CharBlock(required=False)),
             ('hide_title', blocks.BooleanBlock(default=False, required=False)),
             ('embed', EmbedBlock(required=False)),
+            ('sdg', blocks.ChoiceBlock(required=False, choices=sdg_choices,
+                                       label='SDG Goal')),
             ('source', blocks.RichTextBlock(
                 features=['link'], required=False)),
         ],
@@ -167,6 +178,8 @@ class IndicatorWidgetsBlock(blocks.StreamBlock):
             ('title', blocks.CharBlock(required=False)),
             ('hide_title', blocks.BooleanBlock(default=False, required=False)),
             ('document', DocumentChooserBlock(required=False)),
+            ('sdg', blocks.ChoiceBlock(required=False, choices=sdg_choices,
+                                       label='SDG Goal')),
             ('source', blocks.RichTextBlock(
                 features=['link'], required=False)),
         ],
@@ -182,6 +195,7 @@ class IndicatorWidgetsBlock(blocks.StreamBlock):
             ('hide_title', blocks.BooleanBlock(default=False, required=False)),
             ('image', ImageChooserBlock(required=False)),
             ('caption', blocks.TextBlock(required=False)),
+            ('sdg', blocks.ChoiceBlock(required=False, choices=sdg_choices, label='SDG Goal')),
             ('source', blocks.RichTextBlock(
                 features=['link'], required=False)),
         ],
@@ -196,6 +210,8 @@ class IndicatorWidgetsBlock(blocks.StreamBlock):
             ('title', blocks.CharBlock(required=False)),
             ('hide_title', blocks.BooleanBlock(default=False, required=False)),
             ('raw_html', blocks.RawHTMLBlock(required=False)),
+            ('sdg', blocks.ChoiceBlock(required=False, choices=sdg_choices,
+                                       label='SDG Goal')),
             ('source', blocks.RichTextBlock(
                 features=['link'], required=False)),
         ],
@@ -219,6 +235,8 @@ class IndicatorWidgetsBlock(blocks.StreamBlock):
                                                     ('TZ', 'Tanzania'),
                                                 ],
                                                 label='Country')),
+            ('sdg', blocks.ChoiceBlock(required=False, choices=sdg_choices,
+                                       label='SDG Goal')),
             ('data_id', blocks.ChoiceBlock(required=True,
                                            choices=HURUMAP_DATA_DISTS,
                                            label='Data')),
@@ -262,6 +280,8 @@ class IndicatorWidgetsBlock(blocks.StreamBlock):
             ('title', blocks.CharBlock(required=False)),
             ('hide_title', blocks.BooleanBlock(default=False, required=False)),
             ('entities', blocks.ListBlock(EntityStructBlock())),
+            ('sdg', blocks.ChoiceBlock(required=False, choices=sdg_choices,
+                                       label='SDG Goal')),
             ('source', blocks.RichTextBlock(
                 features=['link'], required=False)),
         ],
--- takwimu/views.py
+++ takwimu/views.py
@@ -6,8 +6,10 @@ from operator import itemgetter
 
 from collections import OrderedDict
 from django.conf import settings
+from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response, render
 from django.template import RequestContext
+from django.utils.text import slugify
 from django.views.generic import TemplateView, FormView, View
 from django.views.generic.base import TemplateView
 from wazimap.views import HomepageView, GeographyDetailView
@@ -18,6 +20,7 @@ from forms import SupportServicesContactForm
 
 from sdg import SDG
 from takwimu.search.takwimu_search import TakwimuTopicSearch
+from takwimu.search.utils import get_page_details
 
 
 class HomePageView(TemplateView):
@@ -71,6 +74,21 @@ class SDGIndicatorView(TemplateView):
 
     template_name = 'takwimu/sdg_topic_page.html'
 
+    """
+    {'zero-hunger': {
+        'kenya': {'id': '32dsdfdf4', 'url': '/profiles/kenya/sector-policies-and-programming/'},
+        'tanzania': {'id': '32dsdfdf4', 'url': '/profiles/tanzania/sector-policies-and-programming/'},
+        'uganda': {'id': '32dsdfdf4', 'url': '/profiles/uganda/sector-policies-and-programming/'},
+        },
+      'no-poverty': {
+        'kenya': {'id': '32dsdfdf4', 'url': '/profiles/kenya/sector-policies-and-programming/'},
+        'tanzania': {'id': '32dsdfdf4', 'url': '/profiles/tanzania/sector-policies-and-programming/'},
+        'uganda': {'id': '32dsdfdf4', 'url': '/profiles/uganda/sector-policies-and-programming/'},
+        },
+    }
+    """
+    indicator_sdg_map = {}
+
     def get_context_data(self, **kwargs):
         json_data = open('takwimu/fixtures/sdg.json')
         data = json.load(json_data)
@@ -78,6 +96,53 @@ class SDGIndicatorView(TemplateView):
             **kwargs)
         context['sdgs'] = data
         return context
+
+    def get(self, request, *args, **kwargs):
+        sdg = request.GET.get('sdg', '')
+        country = request.GET.get('country', '')
+
+
+        self.create_indicator_sdg_map()
+
+        if sdg and country:
+            link = self.get_indicator_link(sdg, country)
+            if not link:
+                # return to sdg page and add a flag to show a pop up that no
+                # indicators matching that SDG were found
+                context = self.get_context_data()
+                context['sdg_found'] = False
+                return render(request, self.template_name, context)
+            else:
+                # follow link to profiles page
+                return HttpResponseRedirect(link)
+        return super(SDGIndicatorView, self).get(request, *args, **kwargs)
+
+    def get_indicator_link(self, sdg, country):
+        try:
+            indicator = self.indicator_sdg_map[sdg][country]
+            link = indicator['url'] + '#' +  indicator['topic_title']
+            return link
+        except KeyError:
+            return None
+
+    def create_indicator_sdg_map(self):
+        for page in ProfileSectionPage.objects.live():
+            country, _, _ = get_page_details(page)
+            country = slugify(country)
+            url = page.url
+            try:
+                for topic in page.body.stream_data:
+
+                    topic_title = slugify(topic['value']['title'])
+                    for indicator in topic['value']['indicators']:
+                        indicator_id = indicator['id']
+                        for widget in indicator['value']['widgets']:
+                            widget_id = widget['id']
+                            sdg = widget['value']['sdg']
+                            self.indicator_sdg_map[sdg] = {}
+                            self.indicator_sdg_map[sdg][country] = {'topic_title': topic_title, 'url': url, 'indicator_id': indicator_id}
+            except KeyError:
+                pass
 
 
 def handler404(request):
