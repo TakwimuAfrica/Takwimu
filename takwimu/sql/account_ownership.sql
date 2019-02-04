22dabde7df5c164f70060df284e0002f0aae3f43
Phillip Ahereza <pahereza@gmail.com>
2019-02-04 14:25:37 +0300

    updated sql files to match qualifiers

--- takwimu/sql/account_ownership.sql
+++ takwimu/sql/account_ownership.sql
@@ -38,40 +38,40 @@ CREATE TABLE public.account_ownership (
 --
 
 COPY public.account_ownership (geo_level, geo_code, geo_version, sex, account_ownership_year, total) FROM stdin;
-country	ET	2009	female	2017	29
-country	ET	2009	female	2014	21
-country	KE	2009	female	2017	78
-country	KE	2009	female	2014	71
-country	KE	2009	female	2011	39
-country	NG	2009	female	2017	27
-country	NG	2009	female	2014	34
-country	NG	2009	female	2011	26
-country	SN	2009	female	2017	38
-country	SN	2009	female	2014	11
-country	SN	2009	female	2011	5
-country	TZ	2009	female	2017	42
-country	TZ	2009	female	2014	34
-country	TZ	2009	female	2011	14
-country	ZA	2009	female	2017	70
-country	ZA	2009	female	2014	70
-country	ZA	2009	female	2011	51
-country	ET	2009	male	2017	41
-country	ET	2009	male	2014	23
-country	KE	2009	male	2017	86
-country	KE	2009	male	2014	79
-country	KE	2009	male	2011	46
-country	NG	2009	male	2017	51
-country	NG	2009	male	2014	54
-country	NG	2009	male	2011	33
-country	SN	2009	male	2017	47
-country	SN	2009	male	2014	20
-country	SN	2009	male	2011	6
-country	TZ	2009	male	2017	52
-country	TZ	2009	male	2014	45
-country	TZ	2009	male	2011	21
-country	ZA	2009	male	2017	68
-country	ZA	2009	male	2014	70
-country	ZA	2009	male	2011	56
+country	ET	2009	F	2017	29
+country	ET	2009	F	2014	21
+country	KE	2009	F	2017	78
+country	KE	2009	F	2014	71
+country	KE	2009	F	2011	39
+country	NG	2009	F	2017	27
+country	NG	2009	F	2014	34
+country	NG	2009	F	2011	26
+country	SN	2009	F	2017	38
+country	SN	2009	F	2014	11
+country	SN	2009	F	2011	5
+country	TZ	2009	F	2017	42
+country	TZ	2009	F	2014	34
+country	TZ	2009	F	2011	14
+country	ZA	2009	F	2017	70
+country	ZA	2009	F	2014	70
+country	ZA	2009	F	2011	51
+country	ET	2009	M	2017	41
+country	ET	2009	M	2014	23
+country	KE	2009	M	2017	86
+country	KE	2009	M	2014	79
+country	KE	2009	M	2011	46
+country	NG	2009	M	2017	51
+country	NG	2009	M	2014	54
+country	NG	2009	M	2011	33
+country	SN	2009	M	2017	47
+country	SN	2009	M	2014	20
+country	SN	2009	M	2011	6
+country	TZ	2009	M	2017	52
+country	TZ	2009	M	2014	45
+country	TZ	2009	M	2011	21
+country	ZA	2009	M	2017	68
+country	ZA	2009	M	2014	70
+country	ZA	2009	M	2011	56
 \.
 
 
