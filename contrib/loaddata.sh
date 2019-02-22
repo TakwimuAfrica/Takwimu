b34977037c538d76f57c1e68142248f46e5096a6
Phillip Ahereza <pahereza@gmail.com>
2019-02-22 10:08:53 +0300

    fix fixtures name

--- contrib/loaddata.sh
+++ contrib/loaddata.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cat takwimu/sql/*.sql | psql            # Load tables / data into DB (TBD)
-python manage.py loaddata takwimu/fixtures/takwimu_wazimap_fixtures.json # Load fixtures
+python manage.py loaddata takwimu/fixtures/wazimap.json # Load fixtures
