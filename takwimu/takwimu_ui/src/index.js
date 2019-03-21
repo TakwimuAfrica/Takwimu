c9067d2569806fcf387cb85972bced2cf4a2f0e9
Karim Kawambwa <4308339+karimkawambwa@users.noreply.github.com>
2019-03-21 14:05:42 +0300

    add linting + packages

--- takwimu/takwimu_ui/src/index.js
+++ takwimu/takwimu_ui/src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+
+import withRoot from './withRoot';
 
 const renderApp = (Component, id) => {
     const el = document.getElementById(id);
     if (el) {
+        const App = withRoot(Component);
+
         ReactDOM.render(<App />, el);
     }
 };
