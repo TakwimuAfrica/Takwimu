b6dd310b823d8c2228dd341de1505f3434aed886
Karim Kawambwa <4308339+karimkawambwa@users.noreply.github.com>
2019-03-22 07:54:18 +0300

    remove link to create-react-app dev until images can be resolved

--- takwimu/context_processors.py
+++ takwimu/context_processors.py
@@ -160,31 +160,20 @@ def sdgs(request):
 def asset_manifest(request):
     manifest_filepath = os.path.join(settings.BASE_DIR, 'takwimu/takwimu_ui/build/asset-manifest.json')
     asset_manifest = {}
+    try:
+        with open(manifest_filepath) as f:
+            asset_manifest_contents = json.load(f)
+            for key, value in asset_manifest_contents.viewitems():
+                # Strip starting `/static/` from values
+                if key == 'main.js':
+                    asset_manifest['main'] = value[8:]
+                elif key == 'runtime~main.js':
+                    asset_manifest['runtime'] = value[8:]
+                elif key.startswith('static/js/1') and key.endswith('chunk.js'):
+                    asset_manifest['vendor'] = value[8:]
 
-    if os.environ.get('DEBUG', 'True') == 'True':
-        asset_manifest = {
-            'debug': True,
-            'main': 'js/main.chunk.js',
-            'vendor': 'js/bundle.js',
-            'runtime0': 'js/0.chunk.js',
-            'runtime1': 'js/1.chunk.js',
-            'port': os.environ.get('TAKWIMU_REACT_PORT', '3000')
-        }
-    else:
-        try:
-            with open(manifest_filepath) as f:
-                asset_manifest_contents = json.load(f)
-                for key, value in asset_manifest_contents.viewitems():
-                    # Strip starting `/static/` from values
-                    if key == 'main.js':
-                        asset_manifest['main'] = value[8:]
-                    elif key == 'runtime~main.js':
-                        asset_manifest['runtime'] = value[8:]
-                    elif key.startswith('static/js/1') and key.endswith('chunk.js'):
-                        asset_manifest['vendor'] = value[8:]
-
-        except Exception as e:
-            print(e.message)
+    except Exception as e:
+        print(e.message)
 
     return {
         'asset_manifest': asset_manifest
