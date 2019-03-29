38329ee0ea422b27a7c3a9d7eda34e553ace29cf
Phillip Ahereza <pahereza@gmail.com>
2019-03-29 08:50:52 +0300

    fix styles

--- takwimu/takwimu_ui/src/components/MakingOfTakwimu.js
+++ takwimu/takwimu_ui/src/components/MakingOfTakwimu.js
@@ -1,35 +1,38 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import { Typography } from '@material-ui/core';
 
+import { withStyles } from '@material-ui/core/styles';
 import Section from './Section';
 
-function MakingOfTakwimu() {
-  const styles = {
-    videoWrapper: {
-      position: 'relative',
-      paddingBottom: '56.25%',
-      paddingTop: '25px',
-      marginTop: '25px',
-      height: 0
-    },
-    youtubeIframe: {
-      position: 'absolute',
-      top: 0,
-      left: 0,
-      width: '100%',
-      height: '100%'
-    }
-  };
+const styles = () => ({
+  videoWrapper: {
+    position: 'relative',
+    paddingBottom: '56.25%',
+    paddingTop: '25px',
+    marginTop: '25px',
+    height: 0
+  },
+  youtubeIframe: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%'
+  }
+});
+
+function MakingOfTakwimu({ classes }) {
   return (
     <Section title="The Making of Takwimu" variant="h3">
       <Typography variant="body1">
         Lorem ipsum dolor sit amet, adipiscing elitauris con lorem ipsum dolor
         sit amet, consectetur adipiscing elit.
       </Typography>
-      <div style={styles.videoWrapper}>
+      <div className={classes.videoWrapper}>
         <iframe
-          style={styles.youtubeIframe}
+          className={classes.youtubeIframe}
           title="Making of Takwimu"
           width="560"
           height="315"
@@ -43,4 +46,8 @@ function MakingOfTakwimu() {
   );
 }
 
-export default MakingOfTakwimu;
+MakingOfTakwimu.propTypes = {
+  classes: PropTypes.shape({}).isRequired
+};
+
+export default withStyles(styles)(MakingOfTakwimu);
