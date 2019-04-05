e818ba5166d99df1a0b5e74af553825d6daacb42
Karim Kawambwa <4308339+karimkawambwa@users.noreply.github.com>
2019-04-05 06:17:12 +0300

    fixed px->rem and cover align with brand

--- takwimu/takwimu_ui/src/components/ProfileDetail/index.js
+++ takwimu/takwimu_ui/src/components/ProfileDetail/index.js
@@ -15,6 +15,7 @@ import {
 } from '@material-ui/core';
 
 import classNames from 'classnames';
+import Layout from '../Layout';
 
 import searchIcon from '../../assets/images/icon-search.svg';
 import downArrowBlack from '../../assets/images/down-arrow-black.svg';
@@ -25,42 +26,48 @@ const flagSrc = require.context('../../assets/images/flags', false, /\.svg$/);
 
 const styles = theme => ({
   root: {
-    position: 'relative',
-    height: '544px',
-    padding: '23px',
-    backgroundColor: '#ffffff',
+    width: '100%',
+    height: '34rem',
+    padding: '1.438rem',
+    backgroundColor: 'rgba(255, 255, 255, 0.63)',
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'flex-start',
     justifyContent: 'space-between',
     [theme.breakpoints.up('md')]: {
-      position: 'absolute',
-      width: '374px',
-      top: '99px',
-      left: '99px',
-      zIndex: '1',
+      width: '23.375rem',
       border: 'solid 0.063rem rgba(0, 0, 0, 0.19)',
       borderRadius: '0 0 1.063rem 1.063rem'
     }
   },
+  layout: {
+    position: 'relative',
+    height: '34rem',
+    display: 'flex',
+    justifyContent: 'flex-start',
+    [theme.breakpoints.up('md')]: {
+      position: 'absolute',
+      zIndex: '1'
+    }
+  },
   label: {
-    fontSize: '15px',
+    fontSize: '0.938rem',
     fontWeight: '600'
   },
   verticalLine: {
-    width: '4px',
-    height: '194px',
-    marginLeft: '17px',
-    marginRight: '35px',
+    width: '0.25rem',
+    height: '12.125rem',
+    marginLeft: '1.063rem',
+    marginRight: '2.188rem',
     backgroundColor: theme.palette.primary.main
   },
   countryName: {
-    marginLeft: '18px',
-    marginRight: '12px'
+    marginLeft: '1.125rem',
+    marginRight: '0.75rem'
   },
   chooserButton: {
-    marginTop: '15px',
-    marginBottom: '22px'
+    marginTop: '0.938rem',
+    marginBottom: '1.375rem'
   },
   changeCountryLabel: {
     color: '#848484'
@@ -70,29 +77,29 @@ const styles = theme => ({
     fontWeight: 'unset'
   },
   detail: {
-    fontSize: '32px',
+    fontSize: '2rem',
     fontWeight: '600',
     color: '#231f20'
   },
   datasetName: {
-    fontSize: '15px',
+    fontSize: '0.938rem',
     textDecoration: 'underline',
-    marginRight: '10px',
-    marginLeft: '4px'
+    marginRight: '0.625rem',
+    marginLeft: '0.25rem'
   },
   searchBar: {
     position: 'relative',
     width: '100%'
   },
   searchBarInput: {
-    padding: '10px',
-    borderRadius: '4px',
-    border: 'solid 1px rgba(151, 151, 151, 0.3)'
+    padding: '0.625rem',
+    borderRadius: '0.25rem',
+    border: 'solid 0.063rem rgba(151, 151, 151, 0.3)'
   },
   searchBarIcon: {
     position: 'absolute',
-    right: '16px',
-    top: '16px'
+    right: '1rem',
+    top: '1rem'
   },
   popperIndex: {
     zIndex: 2
@@ -172,174 +179,197 @@ class ProfileDetail extends React.Component {
     const { active: activeRelease } = primaryReleases;
     const { square_kms: squarekms, full_name: fullName } = geography.this;
     return (
-      <div className={classes.root}>
-        <div>
-          <Typography
-            variant="caption"
-            className={classNames([classes.label, classes.changeCountryLabel])}
-          >
-            Change Country
-          </Typography>
-
-          <ButtonBase
-            disableRipple
-            disableTouchRipple
-            style={{ outline: 'none' }}
-            className={classes.chooserButton}
-            onClick={window.toggleDrawer('topic')}
-          >
-            <img alt="" height="37" src={flagSrc('./south-africa.svg')} />
-            <Typography variant="subtitle1" className={classes.countryName}>
-              {fullName}
-            </Typography>
-            <img alt="" src={downArrow} />
-          </ButtonBase>
-        </div>
-
-        <Grid container direction="row" wrap="nowrap">
-          <Grid item>
-            <div className={classes.verticalLine} />
-          </Grid>
-          <Grid item container direction="column" justify="space-between">
-            {population && (
-              <Grid item>
-                <Typography
-                  variant="body1"
-                  className={classNames([classes.label, classes.detailLabel])}
-                >
-                  Population
-                </Typography>
-                <Typography variant="body1" className={classes.detail}>
-                  {Number(population).toLocaleString()}
-                </Typography>
-              </Grid>
-            )}
-            {squarekms && (
-              <Grid item>
-                <Typography
-                  variant="body1"
-                  className={classNames([classes.label, classes.detailLabel])}
-                >
-                  Square kilometres
-                </Typography>
-                <Typography variant="body1" className={classes.detail}>
-                  {Number(squarekms).toLocaleString()}
-                </Typography>
-              </Grid>
-            )}
-            {populationDensity && (
-              <Grid item>
-                <Typography
-                  variant="body1"
-                  className={classNames([classes.label, classes.detailLabel])}
-                >
-                  People per square kilometre
-                </Typography>
-                <Typography variant="body1" className={classes.detail}>
-                  {Number(populationDensity).toLocaleString()}
-                </Typography>
-              </Grid>
-            )}
-          </Grid>
-        </Grid>
-
-        <Grid container>
-          <Typography className={classes.label}>Release:</Typography>
-          {activeRelease && (
-            <ButtonBase
-              disableRipple
-              disableTouchRipple
-              style={{ outline: 'none' }}
-              buttonRef={node => {
-                this.releasesButton = node;
-              }}
-              onClick={this.toggleReleasesMenu}
-            >
-              <Typography className={classes.datasetName}>
-                {activeRelease.name} {activeRelease.year}
+      <Grid container justify="center">
+        <Layout classes={{ root: classes.layout }}>
+          <div className={classes.root}>
+            <div>
+              <Typography
+                variant="caption"
+                className={classNames([
+                  classes.label,
+                  classes.changeCountryLabel
+                ])}
+              >
+                Change Country
               </Typography>
-              <img
-                alt=""
-                src={releasesMenuIsOpen ? upArrowBlack : downArrowBlack}
-              />
-            </ButtonBase>
-          )}
 
-          <Popper
-            className={classes.popperIndex}
-            open={releasesMenuIsOpen}
-            anchorEl={this.releasesButton}
-            style={{
-              width: this.releasesButton
-                ? this.releasesButton.clientWidth
-                : null
-            }}
-          >
-            <ClickAwayListener onClickAway={this.toggleReleasesMenu}>
-              <Paper>
-                <MenuList>
-                  <MenuItem
-                    component="a"
-                    href={`?release=${primaryReleases.active.year}`}
-                    className={classes.releasesMenuItem}
-                  >
-                    {primaryReleases.active.name} {primaryReleases.active.year}
-                  </MenuItem>
-                  {primaryReleases.other.length
-                    ? primaryReleases.other.map(release => (
-                        <MenuItem
-                          key={release.year}
-                          component="a"
-                          href={`?release=${release.year}`}
-                          className={classes.releasesMenuItem}
-                        >
-                          {release.name} {release.year}
-                        </MenuItem>
-                      ))
-                    : null}
-                </MenuList>
-              </Paper>
-            </ClickAwayListener>
-          </Popper>
-        </Grid>
+              <ButtonBase
+                disableRipple
+                disableTouchRipple
+                style={{ outline: 'none' }}
+                className={classes.chooserButton}
+                onClick={window.toggleDrawer('topic')}
+              >
+                <img alt="" height="37" src={flagSrc('./south-africa.svg')} />
+                <Typography variant="subtitle1" className={classes.countryName}>
+                  {fullName}
+                </Typography>
+                <img alt="" src={downArrow} />
+              </ButtonBase>
+            </div>
 
-        <Grid container>
-          <div
-            ref={node => {
-              this.searchBarRef = node;
-            }}
-            className={classes.searchBar}
-          >
-            <Input
-              fullWidth
-              disableUnderline
-              className={classes.searchBarInput}
-              onFocus={this.handleSearch}
-              onBlur={this.hideSearchResults}
-              placeholder="Compare with"
-              onChange={this.handleSearch}
-            />
-            <img alt="" src={searchIcon} className={classes.searchBarIcon} />
+            <Grid container direction="row" wrap="nowrap">
+              <Grid item>
+                <div className={classes.verticalLine} />
+              </Grid>
+              <Grid item container direction="column" justify="space-between">
+                {population && (
+                  <Grid item>
+                    <Typography
+                      variant="body1"
+                      className={classNames([
+                        classes.label,
+                        classes.detailLabel
+                      ])}
+                    >
+                      Population
+                    </Typography>
+                    <Typography variant="body1" className={classes.detail}>
+                      {Number(population).toLocaleString()}
+                    </Typography>
+                  </Grid>
+                )}
+                {squarekms && (
+                  <Grid item>
+                    <Typography
+                      variant="body1"
+                      className={classNames([
+                        classes.label,
+                        classes.detailLabel
+                      ])}
+                    >
+                      Square kilometres
+                    </Typography>
+                    <Typography variant="body1" className={classes.detail}>
+                      {Number(squarekms).toLocaleString()}
+                    </Typography>
+                  </Grid>
+                )}
+                {populationDensity && (
+                  <Grid item>
+                    <Typography
+                      variant="body1"
+                      className={classNames([
+                        classes.label,
+                        classes.detailLabel
+                      ])}
+                    >
+                      People per square kilometre
+                    </Typography>
+                    <Typography variant="body1" className={classes.detail}>
+                      {Number(populationDensity).toLocaleString()}
+                    </Typography>
+                  </Grid>
+                )}
+              </Grid>
+            </Grid>
+
+            <Grid container>
+              <Typography className={classes.label}>Release:</Typography>
+              {activeRelease && (
+                <ButtonBase
+                  disableRipple
+                  disableTouchRipple
+                  style={{ outline: 'none' }}
+                  buttonRef={node => {
+                    this.releasesButton = node;
+                  }}
+                  onClick={this.toggleReleasesMenu}
+                >
+                  <Typography className={classes.datasetName}>
+                    {activeRelease.name} {activeRelease.year}
+                  </Typography>
+                  <img
+                    alt=""
+                    src={releasesMenuIsOpen ? upArrowBlack : downArrowBlack}
+                  />
+                </ButtonBase>
+              )}
+
+              <Popper
+                className={classes.popperIndex}
+                open={releasesMenuIsOpen}
+                anchorEl={this.releasesButton}
+                style={{
+                  width: this.releasesButton
+                    ? this.releasesButton.clientWidth
+                    : null
+                }}
+              >
+                <ClickAwayListener onClickAway={this.toggleReleasesMenu}>
+                  <Paper>
+                    <MenuList>
+                      <MenuItem
+                        component="a"
+                        href={`?release=${primaryReleases.active.year}`}
+                        className={classes.releasesMenuItem}
+                      >
+                        {primaryReleases.active.name}{' '}
+                        {primaryReleases.active.year}
+                      </MenuItem>
+                      {primaryReleases.other.length
+                        ? primaryReleases.other.map(release => (
+                            <MenuItem
+                              key={release.year}
+                              component="a"
+                              href={`?release=${release.year}`}
+                              className={classes.releasesMenuItem}
+                            >
+                              {release.name} {release.year}
+                            </MenuItem>
+                          ))
+                        : null}
+                    </MenuList>
+                  </Paper>
+                </ClickAwayListener>
+              </Popper>
+            </Grid>
+
+            <Grid container>
+              <div
+                ref={node => {
+                  this.searchBarRef = node;
+                }}
+                className={classes.searchBar}
+              >
+                <Input
+                  fullWidth
+                  disableUnderline
+                  className={classes.searchBarInput}
+                  onFocus={this.handleSearch}
+                  onBlur={this.hideSearchResults}
+                  placeholder="Compare with"
+                  onChange={this.handleSearch}
+                />
+                <img
+                  alt=""
+                  src={searchIcon}
+                  className={classes.searchBarIcon}
+                />
+              </div>
+
+              <Popper
+                className={classes.popperIndex}
+                open={showSearchResults}
+                anchorEl={this.searchBarRef}
+                style={{
+                  width: this.searchBarRef
+                    ? this.searchBarRef.clientWidth
+                    : null
+                }}
+              >
+                <Paper>
+                  <MenuList>
+                    <MenuItem>Example</MenuItem>
+                  </MenuList>
+                </Paper>
+              </Popper>
+            </Grid>
+
+            <Button fullWidth>Read the full country analysis</Button>
           </div>
-
-          <Popper
-            className={classes.popperIndex}
-            open={showSearchResults}
-            anchorEl={this.searchBarRef}
-            style={{
-              width: this.searchBarRef ? this.searchBarRef.clientWidth : null
-            }}
-          >
-            <Paper>
-              <MenuList>
-                <MenuItem>Example</MenuItem>
-              </MenuList>
-            </Paper>
-          </Popper>
-        </Grid>
-
-        <Button fullWidth>Read the full country analysis</Button>
-      </div>
+        </Layout>
+      </Grid>
     );
   }
 }
