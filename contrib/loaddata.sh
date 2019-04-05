d1e91ed7754057ff6953b943c9205f52fcab1a6b
Phillip Ahereza <pahereza@gmail.com>
2019-04-05 12:21:18 +0300

    add index page fixtures to loaddata.sh

--- contrib/loaddata.sh
+++ contrib/loaddata.sh
@@ -2,3 +2,4 @@
 
 cat takwimu/sql/*.sql | psql            # Load tables / data into DB (TBD)
 python manage.py loaddata takwimu/fixtures/wazimap.json # Load fixtures
+python manage.py loaddata takwimu/fixtures/indexpage.json # Load fixtures
