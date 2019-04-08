1e95440ebe21f52412eb8927f6c2989eb02a7075
_ Kilemensi <kilemensi@gmail.com>
2019-04-08 18:10:09 +0300

    Remove indexpage fixture + code fix

--- contrib/loaddata.sh
+++ contrib/loaddata.sh
@@ -2,4 +2,3 @@
 
 cat takwimu/sql/*.sql | psql            # Load tables / data into DB (TBD)
 python manage.py loaddata takwimu/fixtures/wazimap.json # Load fixtures
-python manage.py loaddata takwimu/fixtures/indexpage.json # Load fixtures
