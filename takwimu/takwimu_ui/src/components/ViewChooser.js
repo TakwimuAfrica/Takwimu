0211e8f524c51a1b74e3d4ba6f273e705228a8db
KhadijaMahanga <khadijamahanga@y7mail.com>
2019-04-10 17:02:50 +0300

    change textfield to select form control

--- takwimu/takwimu_ui/src/components/ViewChooser.js
+++ takwimu/takwimu_ui/src/components/ViewChooser.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { PropTypes } from 'prop-types';
 import {
+  Grid,
   Typography,
   withStyles,
-  InputAdornment,
-  IconButton,
+  Select,
+  FormControl,
+  OutlinedInput,
   Popper,
   Paper,
   MenuList,
@@ -12,8 +14,8 @@ import {
   TextField
 } from '@material-ui/core';
 
-import downArrow from '../assets/images/down-arrow.svg';
-import upArrow from '../assets/images/up-arrow.svg';
+import KeyboardArrowDown from '@material-ui/icons/KeyboardArrowDown';
+import KeyboardArrowUp from '@material-ui/icons/KeyboardArrowUp';
 
 import Section from './Section';
 
@@ -25,27 +27,41 @@ const styles = theme => ({
     paddingBottom: '3.5rem',
     paddingLeft: '2.8125rem',
     paddingRight: '2.8125rem'
+  },
+  button: {
+    borderRadius: 0,
+    padding: 0
+  },
+  keyboardArrow: {
+    color: '#29a87c'
+  },
+  formControl: {
+    width: '258px'
+  },
+  menu: {
+    paddingTop: 0,
+    backgroundColor: 'red'
+  },
+  textField: {
+    marginLeft: '0.9375rem',
+    marginRight: '1.0625rem',
+    flexBasis: 258
+  },
+  title: {
+    paddingBottom: '2.375rem'
   }
 });
 
-function DropdownAdornment({ classes, isActive, handleClick }) {
-  return (
-    <InputAdornment position="end">
-      <IconButton onClick={handleClick}>
-        <img
-          className={classes.img}
-          alt="Arrow"
-          src={isActive ? upArrow : downArrow}
-        />
-      </IconButton>
-    </InputAdornment>
-  );
+function DropdownAdornment({ classes, isActive }) {
+  if (isActive) {
+    return <KeyboardArrowUp className={classes.keyboardArrow} />;
+  }
+  return <KeyboardArrowDown className={classes.keyboardArrow} />;
 }
 
 DropdownAdornment.propTypes = {
   classes: PropTypes.shape({}).isRequired,
-  isActive: PropTypes.bool.isRequired,
-  handleClick: PropTypes.func.isRequired
+  isActive: PropTypes.bool.isRequired
 };
 
 const DropdownAction = withStyles(styles)(DropdownAdornment);
@@ -61,6 +77,20 @@ class ViewChooser extends Component {
     };
     this.handleTopicChange = this.handleTopicChange.bind(this);
     this.handleCountryChange = this.handleCountryChange.bind(this);
+    this.handleCountryDropdown = this.handleCountryDropdown.bind(this);
+    this.handleTopicDropdown = this.handleTopicDropdown.bind(this);
+  }
+
+  handleTopicDropdown() {
+    this.setState(prevState => ({
+      topicActive: !prevState.topicActive
+    }));
+  }
+
+  handleCountryDropdown() {
+    this.setState(prevState => ({
+      countryActive: !prevState.countryActive
+    }));
   }
 
   handleTopicChange(event) {
@@ -76,66 +106,103 @@ class ViewChooser extends Component {
   }
 
   render() {
-    const { classes } = this.props;
+    const { classes, countries } = this.props;
     const { topic, country, topicActive, countryActive } = this.state;
+    const sortedCountries = countries.sort((a, b) => a.name[0] > b.name[0]);
     return (
       <Section classes={{ root: classes.root }}>
-        <Typography variant="body2">
+        <Typography variant="body2" className={classes.title}>
           View analysis or data for a specific country
         </Typography>
-        <div>
+        <Grid container direction="row" display="flex" alignItems="center">
           <Typography variant="body2">Show me</Typography>
+          <FormControl variant="outlined" className={classes.formControl}>
+            <Select
+              value={topic}
+              displayEmpty
+              onChange={this.handleTopicChange}
+              MenuProps={{
+                className: classes.menu
+              }}
+              IconComponent={() => <DropdownAction isActive={topicActive} />}
+              input={
+                <OutlinedInput
+                  name="topic"
+                  id="outlined-age-simple"
+                  classes={{ formControl: classes.formControl }}
+                  endAdornment={<DropdownAction isActive={topicActive} />}
+                />
+              }
+            >
+              <MenuItem value="">Data by topic</MenuItem>
+              <MenuItem value={10}>Ten</MenuItem>
+              <MenuItem value={20}>Twenty</MenuItem>
+              <MenuItem value={30}>Thirty</MenuItem>
+            </Select>
+          </FormControl>
           <TextField
-            select
-            value={topic}
+            autoFocus={false}
             onChange={this.handleTopicChange}
-            displayEmpty
-            name="topic"
-            className={classes.searchBarInput}
+            variant="outlined"
             placeholder="Data by topic"
+            className={classes.textField}
+            SelectProps={{
+              MenuProps: {
+                className: classes.menu
+              }
+            }}
             InputProps={{
               endAdornment: (
                 <DropdownAction
                   isActive={topicActive}
-                  handleClick={this.handleTopicChange}
+                  handleClick={this.handleTopicDropdown}
                 />
               )
             }}
           />
+          <Popper className={classes.popperIndex} open={topicActive}>
+            <Paper>
+              <MenuList>
+                {sortedCountries.map(xcountry => (
+                  <MenuItem>{xcountry.name}</MenuItem>
+                ))}
+              </MenuList>
+            </Paper>
+          </Popper>
           <Typography variant="body2"> for</Typography>
           <TextField
             select
-            value={country}
+            variant="outlined"
             onChange={this.handleCountryChange}
-            displayEmpty
-            name="country"
-            className={classes.searchBarInput}
-            placeholder="Data by topic"
+            className={classes.textField}
+            placeholder="Country"
+            value={country}
             InputProps={{
               endAdornment: (
                 <DropdownAction
                   isActive={countryActive}
-                  handleClick={this.handleCountryChange}
+                  handleClick={this.handleCountryDropdown}
                 />
               )
             }}
           />
 
-          <Popper className={classes.popperIndex}>
+          <Popper className={classes.popperIndex} open={countryActive}>
             <Paper>
               <MenuList>
                 <MenuItem>Example</MenuItem>
               </MenuList>
             </Paper>
           </Popper>
-        </div>
+        </Grid>
       </Section>
     );
   }
 }
 
 ViewChooser.propTypes = {
-  classes: PropTypes.shape({}).isRequired
+  classes: PropTypes.shape({}).isRequired,
+  countries: PropTypes.shape({}).isRequired
 };
 
 export default withStyles(styles)(ViewChooser);
