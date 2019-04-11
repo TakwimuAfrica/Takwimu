44087db15a73767db6279d1998d26d0064bf976a
nerdlet <brndnyokabi@gmail.com>
2019-04-11 15:17:39 +0300

    Add width

--- takwimu/takwimu_ui/src/components/RelatedContent.js
+++ takwimu/takwimu_ui/src/components/RelatedContent.js
@@ -10,7 +10,7 @@ const styles = theme => ({
   link: {
     color: theme.palette.primary.main
   },
-  linkGrid: { height: '18.456rem' }
+  linkGrid: { height: '18.456rem', width: '100%' }
 });
 
 function RelatedContent({ classes }) {
