c277bf90e2c59f2c322153bd0ba1ef1e392ddaf5
nerdlet <brndnyokabi@gmail.com>
2019-04-11 15:16:46 +0300

    Add height

--- takwimu/takwimu_ui/src/components/RelatedContent.js
+++ takwimu/takwimu_ui/src/components/RelatedContent.js
@@ -1,38 +1,86 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-import {
-  Grid,
-  List,
-  ListItem,
-  ListItemText,
-  withStyles
-} from '@material-ui/core';
+import { Grid, Typography, withStyles } from '@material-ui/core';
 
 import Section from './Section';
 
-function RelatedContent() {
+const styles = theme => ({
+  root: { flexGrow: 1 },
+  link: {
+    color: theme.palette.primary.main
+  },
+  linkGrid: { height: '18.456rem' }
+});
+
+function RelatedContent({ classes }) {
   return (
     <Section title="Related Content" variant="h3">
-      <Grid container direction="row" justify="flex-start" alignItems="center">
-        <Grid item xs={5}>
-          <List>
-            <ListItem>
-              <ListItemText primary="Lorem ipsom dolor sit amec" />
-            </ListItem>
-            <ListItem>
-              <ListItemText primary="South Africa Data visuals for politics" />
-            </ListItem>
-            <ListItem>
-              <ListItemText primary="Source 1 lorem ipsumo" />
-            </ListItem>
-            <ListItem>
-              <ListItemText primary="Source 2 lorem ipsum dolor" />
-            </ListItem>
-          </List>
+      <Grid
+        container
+        spacing={24}
+        direction="row"
+        justify="flex-start"
+        alignItems="center"
+      >
+        <Grid item className={classes.linkGrid}>
+          <ul>
+            <a href="/" className={classes.link}>
+              <li>
+                <Typography variant="body2" className={classes.link}>
+                  Lorem ipsom dolor sit amec
+                </Typography>
+              </li>
+            </a>
+            <a href="/" className={classes.link}>
+              <li>
+                <Typography variant="body2" className={classes.link}>
+                  South Africa Data visuals for politics
+                </Typography>
+              </li>
+            </a>
+            <a href="/" className={classes.link}>
+              <li>
+                <Typography variant="body2" className={classes.link}>
+                  Source 1 lorem ipsumo
+                </Typography>
+              </li>
+            </a>
+            <a href="/" className={classes.link}>
+              <li>
+                <Typography variant="body2" className={classes.link}>
+                  Source 2 lorem ipsum dolor
+                </Typography>
+              </li>
+            </a>
+          </ul>
+        </Grid>
+
+        <Grid item className={classes.linkGrid}>
+          <ul>
+            <a href="/" className={classes.link}>
+              <li>
+                <Typography variant="body2" className={classes.link}>
+                  Lorem ipsom dolor sit amec
+                </Typography>
+              </li>
+            </a>
+            <a href="/" className={classes.link}>
+              <li>
+                <Typography variant="body2" className={classes.link}>
+                  South Africa Data visuals for politics
+                </Typography>
+              </li>
+            </a>
+          </ul>
         </Grid>
       </Grid>
     </Section>
   );
 }
 
-export default withStyles(RelatedContent);
+RelatedContent.propTypes = {
+  classes: PropTypes.shape({}).isRequired
+};
+
+export default withStyles(styles)(RelatedContent);
