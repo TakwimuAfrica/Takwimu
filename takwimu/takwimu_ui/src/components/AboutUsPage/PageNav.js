91a697afe97da0e7f1442a5c71813d3a0bdad28a
khadija <khadijamahanga@y7mail.com>
2019-04-24 08:56:49 +0300

    add page navigation

---
+++ takwimu/takwimu_ui/src/components/AboutUsPage/PageNav.js
@@ -0,0 +1,59 @@
+import React from 'react';
+
+import { Link, Typography, withStyles } from '@material-ui/core';
+import { PropTypes } from 'prop-types';
+
+import classNames from 'classnames';
+
+const styles = theme => ({
+  root: {
+    backgroundColor: theme.palette.info.main,
+    padding: '1.125rem 1.625rem'
+  },
+  otherTopicLinks: {
+    '& > button:nth-child(n)': {
+      marginRight: '3.125rem'
+    }
+  },
+  otherTopic: {
+    color: theme.palette.primary.main,
+    textDecoration: 'underline'
+  },
+  label: {
+    color: '#848484',
+    fontSize: '0.813rem',
+    marginBottom: '1rem'
+  }
+});
+
+function PageNav({ classes, currentContent, contentHeadings }) {
+  return (
+    <div className={classes.root}>
+      <Typography className={classes.label}>On this page</Typography>
+      <div className={classes.otherTopicLinks}>
+        {contentHeadings.map(c => (
+          <Link
+            key={c.link}
+            href={c.link}
+            component="button"
+            variant="body2"
+            color={currentContent === c.link ? 'textPrimary' : 'primary'}
+            className={classNames({
+              [classes.otherTopic]: currentContent !== c.link
+            })}
+          >
+            {c.title}
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+PageNav.propTypes = {
+  classes: PropTypes.shape({}).isRequired,
+  currentContent: PropTypes.string.isRequired,
+  contentHeadings: PropTypes.shape({}).isRequired
+};
+
+export default withStyles(styles)(PageNav);
