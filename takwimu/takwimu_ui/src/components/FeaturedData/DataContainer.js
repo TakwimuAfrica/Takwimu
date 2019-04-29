7634c0e84a4ea1ba9c5c567f38b6b44f888bad0f
Karim Kawambwa <4308339+karimkawambwa@users.noreply.github.com>
2019-04-29 14:22:38 +0300

    cleanup

--- takwimu/takwimu_ui/src/components/FeaturedData/DataContainer.js
+++ takwimu/takwimu_ui/src/components/FeaturedData/DataContainer.js
@@ -46,155 +46,135 @@ const styles = theme => ({
   }
 });
 
-function DataContainer({ id, classes, featuredData, widget }) {
+function DataContainer({ id, classes, data }) {
   const [images, setImages] = useState({});
   const [documents, setDocuments] = useState({});
 
   useEffect(() => {
-    if (widget) {
-      if (widget.type === 'entities') {
-        widget.value.entities.forEach(
-          entity =>
-            entity.image &&
-            fetch(`/api/v2/images/${entity.image}`)
-              .then(response => response.json())
-              .then(json =>
-                setImages(prev => ({
-                  ...prev,
-                  [json.id]: json.meta.download_url
-                }))
-              )
-        );
-      }
-      if (widget.type === 'document') {
-        if (widget.value.document) {
-          fetch(`/api/v2/documents/${widget.value.document}`)
+    if (data.type === 'entities') {
+      data.value.entities.forEach(
+        entity =>
+          entity.image &&
+          fetch(`/api/v2/images/${entity.image}`)
             .then(response => response.json())
             .then(json =>
-              setDocuments(prev => ({
+              setImages(prev => ({
                 ...prev,
                 [json.id]: json.meta.download_url
               }))
-            );
-        }
+            )
+      );
+    }
+    if (data.type === 'document') {
+      if (data.value.document) {
+        fetch(`/api/v2/documents/${data.value.document}`)
+          .then(response => response.json())
+          .then(json =>
+            setDocuments(prev => ({
+              ...prev,
+              [json.id]: json.meta.download_url
+            }))
+          );
       }
     }
 
     return () => {};
   }, []);
 
-  const embedCode = featuredData
-    ? `<iframe
+  const handleDownload = () => {
+    if (data.type === 'featured_data') {
+      const iframe = document.getElementById(
+        `cr-embed-country-${data.country}-${data.data_id}`
+      );
+      iframe.contentWindow.domtoimage
+        .toJpeg(iframe.contentDocument.body)
+        .then(dataUrl => {
+          const link = document.createElement('a');
+          link.download = `${data.title}.jpeg`;
+          link.href = dataUrl;
+          link.click();
+        });
+    } else {
+      const el = document.getElementById(`data-indicator-${id}`);
+      const temp = el.style.height;
+      el.style.height = 'unset';
+      domtoimage
+        .toPng(el)
+        .then(dataUrl => {
+          const link = document.createElement('a');
+          link.download = `${data.value.title}.png`;
+          link.href = dataUrl;
+          link.click();
+        })
+        .finally(() => {
+          el.style.height = temp;
+        });
+    }
+  };
+
+  const embedCode =
+    data.type === 'featured_data'
+      ? `<iframe
     allowFullScreen
-    title="${featuredData.title}"
+    title="${data.title}"
     src="/embed/iframe.html?geoID=country-${
-      featuredData.country
-    }&geoVersion=2009&chartDataID=${
-        featuredData.data_id
-      }&dataYear=2011&chartType=${
-        featuredData.chart_type
-      }&chartHeight=300&chartTitle=${
-        featuredData.title
-      }&initialSort=&statType=${featuredData.data_stat_type}"
+      data.country
+    }&geoVersion=2009&chartDataID=${data.data_id}&dataYear=2011&chartType=${
+          data.chart_type
+        }&chartHeight=300&chartTitle=${data.title}&initialSort=&statType=${
+          data.data_stat_type
+        }"
 />`
-    : `${widget.value.raw_html}`;
+      : `${data.value.raw_html}`;
 
   return (
     <div className={classes.root}>
       <div className={classes.dataContainer}>
         <Grid container direction="column" alignItems="center">
-          {featuredData && <IFrame featuredData={featuredData} />}
+          <Typography
+            variant="body1"
+            align="center"
+            className={classes.indicatorTitle}
+          >
+            {data.value.title}
+          </Typography>
 
-          {widget && widget.type === 'html' && (
+          {data.type === 'featured_data' && (
+            <IFrame featuredData={data.value} />
+          )}
+
+          {data.type === 'html' && (
             <div
               id={`data-indicator-${id}`}
               style={{ width: 'min-content', minWidth: '25rem' }}
             >
-              <Typography
-                variant="body1"
-                align="center"
-                className={classes.indicatorTitle}
-              >
-                {widget.value.title}
-              </Typography>
-              <div
-                dangerouslySetInnerHTML={{ __html: widget.value.raw_html }}
-              />
+              <div dangerouslySetInnerHTML={{ __html: data.value.raw_html }} />
             </div>
           )}
 
-          {widget &&
-            widget.type === 'entities' &&
-            widget.value.entities.map(entity => (
+          {data.type === 'entities' &&
+            data.value.entities.map(entity => (
               <div
                 id={`data-indicator-${id}`}
                 style={{ width: '25rem', height: '250px', overflow: 'scroll' }}
               >
-                <Typography
-                  variant="body1"
-                  align="center"
-                  className={classes.indicatorTitle}
-                >
-                  {widget.value.title}
-                </Typography>
                 {entity.image && <img alt="" src={images[entity.image]} />}
                 <div dangerouslySetInnerHTML={{ __html: entity.description }} />
               </div>
             ))}
 
-          {widget && widget.type === 'document' && (
+          {data.type === 'document' && (
             <div id={`data-indicator-${id}`} style={{ width: '25rem' }}>
-              <Typography
-                variant="body1"
-                align="center"
-                className={classes.indicatorTitle}
-              >
-                {widget.value.title}
-              </Typography>
-              {documents[widget.value.document] && (
-                <PDF scale={0.65} file={documents[widget.value.document]} />
+              {documents[data.value.document] && (
+                <PDF scale={0.65} file={documents[data.value.document]} />
               )}
             </div>
           )}
-          <DataActions
-            onDownload={() => {
-              if (featuredData) {
-                const iframe = document.getElementById(
-                  `cr-embed-country-${featuredData.country}-${
-                    featuredData.data_id
-                  }`
-                );
-                iframe.contentWindow.domtoimage
-                  .toJpeg(iframe.contentDocument.body)
-                  .then(dataUrl => {
-                    const link = document.createElement('a');
-                    link.download = `${featuredData.title}.jpeg`;
-                    link.href = dataUrl;
-                    link.click();
-                  });
-              } else {
-                const el = document.getElementById(`data-indicator-${id}`);
-                const temp = el.style.height;
-                el.style.height = 'unset';
-                domtoimage
-                  .toPng(el)
-                  .then(dataUrl => {
-                    const link = document.createElement('a');
-                    link.download = `${widget.value.title}.png`;
-                    link.href = dataUrl;
-                    link.click();
-                  })
-                  .finally(() => {
-                    el.style.height = temp;
-                  });
-              }
-            }}
-            embedCode={embedCode}
-          />
+          <DataActions onDownload={handleDownload} embedCode={embedCode} />
         </Grid>
       </div>
 
-      {featuredData && (
+      {data.type === 'featured_data' && (
         <div className={classes.descriptionContainer}>
           <Grid container direction="row" wrap="nowrap">
             <Grid item>
@@ -202,7 +182,7 @@ function DataContainer({ id, classes, featuredData, widget }) {
             </Grid>
             <Grid item>
               <Typography variant="caption" className={classes.description}>
-                {featuredData.description}
+                {data.value.description}
               </Typography>
             </Grid>
           </Grid>
@@ -215,14 +195,11 @@ function DataContainer({ id, classes, featuredData, widget }) {
 DataContainer.propTypes = {
   id: PropTypes.string,
   classes: PropTypes.shape({}).isRequired,
-  featuredData: PropTypes.shape({}),
-  widget: PropTypes.shape({})
+  data: PropTypes.shape({}).isRequired
 };
 
 DataContainer.defaultProps = {
-  id: '',
-  featuredData: null,
-  widget: null
+  id: ''
 };
 
 export default withStyles(styles)(DataContainer);
