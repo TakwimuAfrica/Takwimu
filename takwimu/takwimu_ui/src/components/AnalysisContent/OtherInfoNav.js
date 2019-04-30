c05f536f2aff5118b7a7d6ce11c757201fb65fca
_ Kilemensi <kilemensi@gmail.com>
2019-04-30 08:35:24 +0300

    Remove alt text on img causing alignment issues

--- takwimu/takwimu_ui/src/components/AnalysisContent/OtherInfoNav.js
+++ takwimu/takwimu_ui/src/components/AnalysisContent/OtherInfoNav.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { PropTypes } from 'prop-types';
+
+import classNames from 'classnames';
 
 import { withStyles } from '@material-ui/core';
-import { PropTypes } from 'prop-types';
 
 import AnalysisContentNavigation from './ContentNavigation';
 
