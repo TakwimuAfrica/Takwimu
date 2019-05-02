6d8fcc854e48c54881aed12c8eced0f112019aef
karimkawambwa <4308339+karimkawambwa@users.noreply.github.com>
2019-05-02 14:27:51 +0300

    add publish signal

--- takwimu/signals.py
+++ takwimu/signals.py
@@ -17,6 +17,32 @@ from wagtail.core.signals import page_published
 from takwimu.search.takwimu_search import TakwimuTopicSearch
 from takwimu.search.utils import get_widget_data, get_page_details
 
+from django.utils.text import slugify
+
+from selenium import webdriver
+from django.conf import settings
+
+@receiver(page_published, sender=ProfilePage)
+@receiver(page_published, sender=ProfileSectionPage)
+def create_downloadable_analysis_pdf(sender, instance, created, **kwargs):
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+    # chrome can not be run as root, which is the case in docker
+    options.add_argument('no-sandbox')
+    options.add_argument('disable-gpu')
+    browser = webdriver.Chrome(options=options)
+
+    server_url = settings.HURUMAP.get('url', 'localhost:8000')
+
+    country = get_page_details(instance)
+
+    section_title = instance.title if isinstance(instance, ProfileSectionPage) else ''
+
+    print("create_daownloadable_analysis")
+    for topic in instance.body.stream_data:
+        topic_id = topic.get('id')
+        browser.get("%s/profiles/%s/%s?contentonly=1#%s" % (server_url, slugify(country), slugify(section_title), topic_id))
+        browser.save_screenshot("%s/pdf.png" % settings.MEDIA_ROOT)
 
 @receiver(page_published, sender=ProfilePage)
 @receiver(page_published, sender=ProfileSectionPage)
