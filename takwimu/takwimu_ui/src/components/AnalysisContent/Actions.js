c4588d9479c10fa228f6deea49bf353920e2b2fb
Karim Kawambwa <4308339+karimkawambwa@users.noreply.github.com>
2019-05-13 17:17:16 +0300

    hare analysis

--- takwimu/takwimu_ui/src/components/AnalysisContent/Actions.js
+++ takwimu/takwimu_ui/src/components/AnalysisContent/Actions.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { ButtonBase, Typography, withStyles } from '@material-ui/core';
+import {
+  ButtonBase,
+  Typography,
+  withStyles,
+  Popover,
+  Paper,
+  ClickAwayListener,
+  Grid
+} from '@material-ui/core';
 import { PropTypes } from 'prop-types';
+
+import { TwitterShareButton, TwitterIcon } from 'react-share';
 
 import shareIcon from '../../assets/images/analysis/share.svg';
 import downloadIcon from '../../assets/images/analysis/download.svg';
@@ -28,10 +38,15 @@ const styles = {
   shareButton: {
     fontSize: '0.813rem',
     color: '#848484'
+  },
+  sharePopover: {
+    width: '20.313rem',
+    padding: '10px'
   }
 };
 
 function Actions({ classes, hideLastUpdated }) {
+  const [shareButtonElement, setShareButtonElement] = useState(null);
   return (
     <div className={classes.root}>
       {!hideLastUpdated && (
@@ -39,7 +54,11 @@ function Actions({ classes, hideLastUpdated }) {
           Last Updated: <strong>18th December 2018</strong>
         </Typography>
       )}
-      <ButtonBase className={classes.shareButton}>
+      <ButtonBase
+        disableRipple
+        className={classes.shareButton}
+        onClick={e => setShareButtonElement(e.target)}
+      >
         <img alt="share" src={shareIcon} className={classes.actionIcon} />
         Share this analysis
       </ButtonBase>
@@ -47,13 +66,31 @@ function Actions({ classes, hideLastUpdated }) {
         <img alt="download" src={downloadIcon} className={classes.actionIcon} />
         Download this analysis (PDF 800kb)
       </ButtonBase>
+
+      <Popover
+        open={shareButtonElement !== null}
+        anchorEl={shareButtonElement}
+        anchorOrigin={{ vertical: -80, horizontal: -80 }}
+      >
+        <ClickAwayListener onClickAway={() => setShareButtonElement(null)}>
+          <Paper className={classes.sharePopover}>
+            <Grid container justify="space-between">
+              <Typography>Share the link to this analysis:</Typography>
+              <TwitterShareButton url={window.location.href}>
+                <TwitterIcon size={40} />
+              </TwitterShareButton>
+            </Grid>
+          </Paper>
+        </ClickAwayListener>
+      </Popover>
     </div>
   );
 }
 
 Actions.propTypes = {
   classes: PropTypes.shape({}).isRequired,
-  hideLastUpdated: PropTypes.bool
+  hideLastUpdated: PropTypes.bool,
+  takwimu: PropTypes.shape({}).isRequired
 };
 
 Actions.defaultProps = {
