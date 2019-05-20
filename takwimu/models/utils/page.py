3336aad3763dc4acad00c99177a791ab67f84a2e
Karim Kawambwa <4308339+karimkawambwa@users.noreply.github.com>
2019-05-20 10:47:04 +0300

    handle hurumap and flourish indicators

--- takwimu/models/utils/page.py
+++ takwimu/models/utils/page.py
@@ -15,11 +15,32 @@ def page_indicators_to_images(page, browser):
             if item["type"] == "indicator":
                 indicator_id = item["id"]
                 widget = item["value"]["widget"][0]
+
+                data = None
                 if widget["type"] == "html":
                     data = widget["value"]
                     browser.get(
                         f"""data:text/html;charset=utf-8,{urllib.parse.quote(data["raw_html"])}""")
+                if widget["type"] == "flourish":
+                    data = widget["value"]
+                    browser.get(
+                        f"""data:text/html;charset=utf-8,<html><body><iframe src="/flourish/{data["html"]}"/></body></html>""")
+                if widget["type"] == "hurumap":
+                    data = widget["value"]
+                    browser.get(
+                        f"""data:text/html;charset=utf-8,<iframe
+                            allowFullScreen
+                            title="${data["title"]}"
+                            src="/embed/iframe.html?geoID=country-${
+                            data["data_country"]
+                            }&geoVersion=2009&chartDataID=${data["data_id"]}&dataYear=2011&chartType=${
+                            data["chart_type"]
+                        }&chartHeight=300&chartTitle=${data["title"]}&initialSort=&statType=${
+                            data["data_stat_type"]
+                        }"
+                        />""")
 
+                if data:
                     # Wait for animation
                     # in some indicators
                     time.sleep(5)
