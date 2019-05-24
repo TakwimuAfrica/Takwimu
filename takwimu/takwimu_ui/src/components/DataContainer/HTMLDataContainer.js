373057edc3df6b067f4dbd0e04196cf3d5d79a3d
karimkawambwa <4308339+karimkawambwa@users.noreply.github.com>
2019-05-24 11:50:05 +0300

    disable html data viz sharing

--- takwimu/takwimu_ui/src/components/DataContainer/HTMLDataContainer.js
+++ takwimu/takwimu_ui/src/components/DataContainer/HTMLDataContainer.js
@@ -4,9 +4,7 @@ import { PropTypes } from 'prop-types';
 
 import { withStyles } from '@material-ui/core';
 
-import domtoimage from 'dom-to-image';
 import DataActions from './DataActions';
-import { shareIndicator, uploadImage } from './common';
 
 const styles = {
   root: {
@@ -16,23 +14,12 @@ const styles = {
 
 function DataContainer({ id, classes, data }) {
   const containerId = `data-indicator-${id}`;
-
-  const handleShare = () => {
-    domtoimage.toPng(document.getElementById(containerId)).then(dataURL => {
-      uploadImage(id, dataURL).then(success => {
-        if (success) {
-          shareIndicator(id);
-        }
-      });
-    });
-  };
-
   return (
     <Fragment>
       <div id={containerId} className={classes.root}>
         <div dangerouslySetInnerHTML={{ __html: data.raw_html }} />
       </div>
-      <DataActions onShare={handleShare} />
+      <DataActions />
     </Fragment>
   );
 }
