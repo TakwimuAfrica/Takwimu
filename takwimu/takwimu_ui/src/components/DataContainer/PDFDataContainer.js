d76950aa14ba8fbf04dc7a8fca28ad671fc0e5c8
Karim Kawambwa <4308339+karimkawambwa@users.noreply.github.com>
2019-05-27 17:43:04 +0300

    [Staging] PDF Document Indicator to download document (#559)
    * download pdf indicator
    
    * test and fix document download
    
    * pdf download share and navigate

--- takwimu/takwimu_ui/src/components/DataContainer/PDFDataContainer.js
+++ takwimu/takwimu_ui/src/components/DataContainer/PDFDataContainer.js
@@ -2,19 +2,27 @@
 import React, { useState, useEffect, Fragment } from 'react';
 import { PropTypes } from 'prop-types';
 
-import { withStyles } from '@material-ui/core';
-
+import { withStyles, ButtonBase } from '@material-ui/core';
 import PDF from 'react-pdf-js';
+import leftArrow from '../../assets/images/left-arrow.svg';
+import rightArrow from '../../assets/images/right-arrow.svg';
+
 import DataActions from './DataActions';
 
 const styles = {
   root: {
-    width: '25rem'
+    display: 'flex',
+    justifyContent: 'center'
+  },
+  pageButton: {
+    margin: '20px'
   }
 };
 
 function DataContainer({ id, classes, data }) {
   const [documents, setDocuments] = useState({});
+  const [page, setPage] = useState(1);
+  const [numberOfPages, setNumberOfPages] = useState(0);
 
   useEffect(() => {
     if (data.document) {
@@ -31,20 +39,56 @@ function DataContainer({ id, classes, data }) {
   }, []);
 
   const handleDownload = () => {
-    // TODO:
+    const link = document.createElement('a');
+    link.href = documents[data.document];
+    link.download = documents[data.document].split('/').pop();
+    link.target = '_blank';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
+  const handleShare = () => {
+    window.open(
+      `https://twitter.com/intent/tweet?url=${escape(documents[data.document])}`
+    );
   };
 
   return (
-    <Fragment>
-      {data.document && (
+    data.document && (
+      <Fragment>
         <div id={`data-indicator-${id}`} className={classes.root}>
+          <ButtonBase
+            disableRipple
+            disableTouchRipple
+            disabled={page <= 1}
+            className={classes.pageButton}
+            onClick={() => setPage(page - 1)}
+          >
+            <img alt="" src={leftArrow} />
+          </ButtonBase>
           {documents[data.document] && (
-            <PDF scale={0.65} file={documents[data.document]} />
+            <PDF
+              scale={1}
+              page={page}
+              file={documents[data.document]}
+              onDocumentComplete={setNumberOfPages}
+            />
           )}
+          <ButtonBase
+            disableRipple
+            disableTouchRipple
+            disabled={page >= numberOfPages}
+            className={classes.pageButton}
+            onClick={() => setPage(page + 1)}
+          >
+            <img alt="" src={rightArrow} />
+          </ButtonBase>
         </div>
-      )}
-      <DataActions onDownload={handleDownload} embedCode="" />
-    </Fragment>
+
+        <DataActions onDownload={handleDownload} onShare={handleShare} />
+      </Fragment>
+    )
   );
 }
 
