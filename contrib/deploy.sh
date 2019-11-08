832ef3843496f59f17db878228e6951191d72711
Clemence Kyara <clemence@codeforafrica.org>
2019-11-08 10:45:35 +0300

    Make git push run always

--- contrib/deploy.sh
+++ contrib/deploy.sh
@@ -1,3 +1,4 @@
+# git push command should run regardless of the output of build and commit
 cd takwimu/takwimu_ui && yarn build && \
-cd ../.. && git add . && (git diff-index --quiet HEAD || (git commit -m "Deploy $(date --iso-8601=minutes)"))
+cd ../.. && git add . && (git diff-index --quiet HEAD || (git commit -m "Deploy $(date --iso-8601=minutes)")); \
 git push -f dokku deploy:master
